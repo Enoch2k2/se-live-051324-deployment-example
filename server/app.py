62bc6eb3429afed1f732e4da7a0741d9637c7455
Enoch2k2 <enoch2k2@hotmail.com>
2024-07-23 11:54:49 -0500

    updated for render deployment

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from config import app
 from models.book import Book
 from models.author import Author
@@ -6,6 +7,10 @@ from models.user import User
 from routes.books import *
 from routes.authors import *
 from routes.sessions import *
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("index.html")
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
